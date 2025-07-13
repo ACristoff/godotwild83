3fc761cd19a1e9d2e1fffa58ff731884f471ffbd
Alex Cristoffanini <acristoffanini@gmail.com>
2025-07-12 17:46:18 -0700

    cursor with grid resource, level with grid resource

---
+++ Lib/Level_Manager/level.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+class_name Level
+
+const DIRECTIONS = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+
+@export var grid: Resource = preload("res://Lib/Grid/grid.tres")
+@export var level_music: AudioStreamMP3
