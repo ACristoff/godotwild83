b1b39e28d23f45edeeef3c5c8ebdcedee30ba988
Alex Cristoffanini <acristoffanini@gmail.com>
2025-07-12 17:50:59 -0700

    defining early level funcs

--- Lib/Level_Manager/level.gd
+++ Lib/Level_Manager/level.gd
@@ -6,3 +6,13 @@ const DIRECTIONS = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
 
 @export var grid: Resource = preload("res://Lib/Grid/grid.tres")
 @export var level_music: AudioStreamMP3
+
+var buildings := {}
+
+
+func reinitialize():
+	for child in get_children():
+		#var unit := child as Unit
+		#if not unit:
+			#continue
+		pass
